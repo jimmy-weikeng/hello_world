ce2124b0f4c56f0be3b1e07f155eb14cc92f7c7b
jimmychen <jimmychen@weikeng.com.tw>
2025-09-08 15:07:31 +0800

    first commit

---
+++ hello_world.c
@@ -0,0 +1,7 @@
+#include <stdio.h>                  // standard I/O header file
+ 
+int main(int argc, char **argv)
+{
+     printf("\nHello World !!!\n");        // print "Hello World" message on screen
+     return(0);
+}
---
+++ test.c
@@ -0,0 +1,2 @@
+test jimmy!
+jimmy modify 2025-09-08
